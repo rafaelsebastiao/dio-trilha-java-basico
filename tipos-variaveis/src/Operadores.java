c9af43a42985f4ae684f23a2c84aae3182a17a3e
Rafael Sebastião <rafael.sebastiao661@gmail.com>
2025-01-29 17:28:03 -0300

    update Operadores.java

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -13,5 +13,15 @@ public class Operadores {
 
         concatenacao = "1" + (1 + 1 + 1); //13
         System.out.println(concatenacao);
+
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero *= -1;
+
+        System.out.println(numero);
     }
 }

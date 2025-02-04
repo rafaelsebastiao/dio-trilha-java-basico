27ccf678a086df6bc9f33457d6011edee49faa3f
rafael <rafael.sebastiao661@gmail.com>
2025-02-03 22:47:41 -0300

    Create AboutMe.java

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobreNome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        //lendo dados
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.printf("Olá, me chamo %s %s\n", nome, sobreNome);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        
+        System.out.print("Digite seu sobrenome: ");
+        String sobreNome = scanner.next();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        //imprimindo dados do usuário
+        System.out.printf("\n\nOlá, me chamo %s %s\n", nome, sobreNome);
         System.out.printf("Tenho %d anos\n", idade);
-        System.out.printf("Minha altura é %.2f cm\n", altura);
-
+        System.out.println("Minha altura é " + altura);
     }
 }

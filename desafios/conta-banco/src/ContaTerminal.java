5da9abd79df69529ee716937a7e3cce1464a470d
rafael <rafael.sebastiao661@gmail.com>
2025-02-04 00:38:37 -0300

    Update ContaTerminal.java

--- desafios/conta-banco/src/ContaTerminal.java
+++ desafios/conta-banco/src/ContaTerminal.java
@@ -1,6 +1,30 @@
 import java.util.Scanner;
 import java.util.Locale;
 
+/**
+*<h1>ContaTerminal</h1>
+*<p>Classe que recebe dados via terminal contendo as características de conta em banco conforme atributos abaixo: </p>
+* 
+
+*<p><strong>Atributo</strong> <strong>Tipo</strong> <strong>Exemplo</strong></p>
+ * <ul>
+ *     <li><strong>Número</strong>: Inteiro (Exemplo: 1021)</li>
+ *     <li><strong>Agência</strong>: Texto (Exemplo: 067-8)</li>
+ *     <li><strong>Nome Cliente</strong>: Texto (Exemplo: MARIO ANDRADE)</li>
+ *     <li><strong>Saldo</strong>: Decimal (Exemplo: 237.48)</li>
+ * </ul>
+ * <p>*</p>
+ * <p>*</p>
+ * <p>*</p> 
+ * 
+ * 
+ * 
+ *@author Rafael Sebastião
+@version 1.0.0
+@since 04/02/2025
+
+ */
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
@@ -8,6 +32,8 @@ public class ContaTerminal {
         System.out.println("Digite o número da agência: ");
         int numero = scanner.nextInt();
 
+
+        //Leitura do buffer "\n"
         scanner.nextLine();
 
         System.out.println("Digite a agência: ");
